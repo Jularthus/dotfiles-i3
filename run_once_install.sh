c8e9fd6944bc83e25ece5858e02571ee79b95dfb
Jules-Arthus KLEIN <jularthus@localhost-live-1.home>
2025-06-14 23:55:29 +0200

    updated install.sh

--- run_once_install.sh
+++ run_once_install.sh
@@ -1,36 +1,75 @@
 #!/bin/bash
 
-echo -e "\e[1;31mInstalling: All packages\e[0m"
-nix profile install nixpkgs#polybar\
-                    nixpkgs#fastfetch\
-                    nixpkgs#bat\
-                    nixpkgs#nodejs\
-                    nixpkgs#lunarvim\
-                    nixpkgs#flameshot\
-                    nixpkgs#ranger\
-                    nixpkgs#gitkraken
+IS_NIX=false
+IS_FEDORA=false
 
-# font (jetbrains)
-echo -e "\e[1;31mInstalling: Fonts\e[0m"
-nix profile install --expr 'with builtins.getFlake("flake:nixpkgs"); legacyPackages.x86_64-linux.nerdfonts.override { fonts = ["JetBrainsMono"]; }' --impure
+if [ -f /etc/os-release ]; then
+  . /etc/os-release
+  case "$ID" in
+    nixos)
+      IS_NIX=true
+      ;;
+    fedora)
+      IS_FEDORA=true
+      ;;
+  esac
+fi
 
-# wallpaper
-echo -e "\e[1;31mApplying: Wallpaper\e[0m"
-/run/current-system/sw/bin/feh --no-fehbg --bg-scale ~/.config/wallpaper/leaves2.png
+install_packages() {
+  echo -e "\e[1;31mInstalling: All packages\e[0m"
+  if $IS_NIX; then
+    nix profile install nixpkgs#polybar \
+                        nixpkgs#fastfetch \
+                        nixpkgs#bat \
+                        nixpkgs#nodejs \
+                        nixpkgs#lunarvim \
+                        nixpkgs#flameshot \
+                        nixpkgs#ranger \
+                        nixpkgs#gitkraken
+  elif $IS_FEDORA; then
+    sudo dnf install -y polybar fastfetch bat nodejs flameshot ranger gitkraken
+    # lunarvim : installation manuelle ?
+  fi
+}
 
-# installation oh my zsh
-echo -e "\e[1;31mInstalling: Oh-my-zsh\e[0m"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-# yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
+install_fonts() {
+  echo -e "\e[1;31mInstalling: Fonts\e[0m"
+  if $IS_NIX; then
+    nix profile install --expr 'with builtins.getFlake("flake:nixpkgs"); legacyPackages.x86_64-linux.nerdfonts.override { fonts = ["JetBrainsMono"]; }' --impure
+  elif $IS_FEDORA; then
+    echo "NEED SUDO FOR INSTALLING FONTS"
+    sudo mkdir -p /usr/local/share/fonts/JetBrainsMono/
+    curl -o "/tmp/JetBrainsMono.zip" "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip" 
+    sudo unzip /tmp/JetBrainsMono.zip -d /usr/local/fonts/JetBrainsMono/
+    # echo "MANUALLY INSTALL THROUGH : https://www.nerdfonts.com/font-downloads"
+  fi
+}
 
-# mount fortune folder
-echo -e "\e[1;31mInstalling: Fortune folder (sshfs from ServeurKlein)\e[0m"
-echo -e "\033[1mDISABLED FOR NOW ; USE fortuneStart INSTEAD\033[0m"
-# sshfs -o reconnect -p 8022 jularthus@jularthus.fr:/home/jularthus/fortune .config/fortune
+apply_wallpaper() {
+  echo -e "\e[1;31mApplying: Wallpaper\e[0m"
+  feh --no-fehbg --bg-scale ~/.config/wallpaper/leaves2.png
+}
 
-# launch polybar
-echo -e "\e[1;31mKilling i3bar\e[0m"
-killall i3bar
-killall polybar
-echo -e "\e[1;31mLaunching: Polybar\e[0m"
-polybar >/dev/null 2>&1 &
+install_ohmyzsh() {
+  echo -e "\e[1;31mInstalling: Oh-my-zsh\e[0m"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+}
+
+mount_fortune() {
+  echo -e "\e[1;31mInstalling: Fortune folder (sshfs from ServeurKlein)\e[0m"
+  echo -e "\033[1mDISABLED FOR NOW ; USE fortuneStart INSTEAD\033[0m"
+  # sshfs -o reconnect -p 8022 jularthus@jularthus.fr:/home/jularthus/fortune ~/.config/fortune
+}
+
+launch_polybar() {
+  echo -e "\e[1;31mRestarting: Polybar (and killing i3bar)\e[0m"
+  killall i3bar polybar 2>/dev/null
+  nohup polybar >/dev/null 2>&1 &
+}
+
+install_packages
+install_fonts
+apply_wallpaper
+install_ohmyzsh
+mount_fortune
+launch_polybar
