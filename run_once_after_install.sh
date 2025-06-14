e36ef2f7163954e9dc0bf103987e54b4dc2315a5
Jularthus <59778433+Jularthus@users.noreply.github.com>
2025-06-15 01:06:36 +0200

    Create run_once_after_install.sh

---
+++ run_once_after_install.sh
@@ -0,0 +1,15 @@
+launch_polybar() {
+  echo -e "\e[1;31mRestarting: Polybar (and killing i3bar)\e[0m"
+  killall i3bar polybar 2>/dev/null
+  nohup polybar >/dev/null 2>&1 &
+}
+
+launch_picom() {
+  echo -e "\e[1;31mLaunching: Picom\e[0m"
+  nohup picom >/dev/null 2>&1 &
+}
+
+launch_polybar
+launch_picom
+
+i3 restart
