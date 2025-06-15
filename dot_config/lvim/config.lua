854b4a54b3b1e6cfc1a52025012d42e22d2d72cf
Jules-Arthus "JA" Klein <jules.arthus.klein@gmail.com>
2025-06-16 00:17:23 +0200

    updated lvim cfg

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -11,8 +11,15 @@ lvim.plugins = {
 require('cord').setup {
   editor = {
     client = "lunarvim",
-    tooltip = "LunarVim 4 the win!",
+    tooltip = "LunarVim 4 the win",
   }
+}
+
+-- Nvim-tree config
+lvim.builtin.nvimtree.setup.actions = {
+  open_file = {
+    quit_on_open = true,
+  },
 }
 
 -- Neoformat configuration
@@ -22,6 +29,30 @@ vim.cmd([[
     autocmd BufWritePre * undojoin | Neoformat
   augroup END
 ]])
+
+-- Disabling JS suggestions
+-- require('lspconfig').tsserver.setup({
+--     init_options = {
+--         preferences = {
+--             disableSuggestions = true,
+--         },
+--     },
+-- })
+
+-- add terminal shortcut
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+terminal",
+  t = { '<cmd>ToggleTerm<CR>', "Open terminal" },
+  d = { '<cmd>TermExec cmd="dd"<CR>', "Open term and run DevDocker" },
+  h = { '<cmd>TermExec cmd="clear" dir=%:p:h<CR>', "Terminal in file dir" },
+  r = { '<cmd>TermExec cmd="clear" dir=$(git rev-parse --show-toplevel)<CR>', "Terminal in project root" },
+}
+
+-- add select shortcuts
+lvim.builtin.which_key.mappings["v"] = {
+  name = "+Select",
+  f = { '[{kVj%', "Select whole function" },
+}
 
 -- formatting
 -- lvim.format_on_save.enabled = true
