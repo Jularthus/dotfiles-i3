c7dc872ca3b50e8cc68811a7a8bddd5ce2aa502a
Jules-Arthus "JA" Klein <jules.arthus.klein@gmail.com>
2025-06-16 19:23:24 +0200

    added powermenu

--- dot_config/rofi/powermenu/type-4/powermenu.sh
+++ dot_config/rofi/powermenu/type-4/powermenu.sh
@@ -11,7 +11,7 @@
 
 # Current Theme
 dir="$HOME/.config/rofi/powermenu/type-4"
-theme='style-5'
+theme='style-2'
 
 # CMDs
 uptime="`uptime -p | sed -e 's/up //g'`"
@@ -29,7 +29,7 @@ no=''
 # Rofi CMD
 rofi_cmd() {
 	rofi -dmenu \
-		-p "Goodbye ${USER}" \
+		-p "Pas longtemps la pause hein..." \
 		-mesg "Uptime: $uptime" \
 		-theme ${dir}/${theme}.rasi
 }
