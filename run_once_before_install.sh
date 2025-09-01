3f96b14a620fb593fac28323678cf2d89925b996
Jularthus <59778433+Jularthus@users.noreply.github.com>
2025-09-01 17:32:43 +0200

    Update package installation in run_once_before_install.sh

--- run_once_before_install.sh
+++ run_once_before_install.sh
@@ -38,10 +38,12 @@ install_packages() {
                         nixpkgs#nodejs \
                         nixpkgs#sshfs \
                         nixpkgs#glow \
-                        # nixpkgs#lunarvim \
                         nixpkgs#flameshot \
                         nixpkgs#ranger \
+                        github:nixos/nixpkgs/c5dd43934613ae0f8ff37c59f61c507c2e8f980d#neovim \
                         nixpkgs#gitkraken --impure
+
+  curl https://raw.githubusercontent.com/LunarVim/LunarVim/refs/heads/master/utils/installer/install.sh | bash -s -- -y
   elif $IS_FEDORA; then
     sudo dnf install -y zsh kitty git sshfs rofi picom polybar pip fastfetch bat nodejs glow flameshot ranger cargo script neovim
     sudo dnf remove --noautoremove -y neovim
